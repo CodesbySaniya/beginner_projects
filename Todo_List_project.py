c0267678e0707c4082f1f6ca985d683159a88998
Saniya Anijwal <sunitaanijwal123@gmail.com>
2026-03-13 23:55:30 +0530

    Update Todo_List_project.py

--- Todo_List_project.py
+++ Todo_List_project.py
@@ -1,8 +1,9 @@
-#project-TO_DO_LIST_PROJECT IN PYTHON
+#project-updated_TO_DO_LIST_PROJECT IN PYTHON
 while True:
   print("1.add task")
   print("2.view task")
   print("3.exit task")
+  print("4.delete task")
   choice=int(input("enter your choice:"))
   if choice==1:
     task=input("enter your task")
@@ -10,9 +11,21 @@ while True:
       f.write(task+"\n")
   elif choice==2:
     with open('to_do_list.txt',"r") as f:
-       task=f.readlines()
-       for i,task in enumerate(task,1):
+       tasks=f.readlines()
+       for i,task in enumerate(tasks,1):
         print(i,task) 
+  elif choice==4:
+    delete_task=int(input("enter the task number you want to delete"))
+    with open("to_do_list.txt","r") as f:
+      tasks=f.readlines()
+      for i,task in enumerate(tasks,1):
+        print(i,task) 
+        # delete_task=int(input("enter the task number you want to delete"))
+        del tasks[delete_task-1]
+        with open('to_do_list.txt',"w") as f:
+         for t in tasks:
+           f.write(t)
+
   elif choice == 3:
     break
 print("thank you for your time")
